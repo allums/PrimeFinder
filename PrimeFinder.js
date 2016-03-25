109364158676ad878c3c2f0aa5e04130ea5a00be
Stephen <stephenlallum@gmail.com>
2016-03-24 19:00:22 -1000

    latest commit

--- PrimeFinder.js
+++ PrimeFinder.js
@@ -1,10 +1,16 @@
-function PrimeFinder(){
+function PrimeFinder(n){
 
-	 this.getPrimeFinder = function(){};
+	 this.getPrimeFinder = function(){
+	 	n = 2;
+	 	if (num / 1 === num && num / num === 1){
+	 		return num;
+	 	}
 
-
-
-
+	 	return false;
+	 };
 }
+
+
+
 
 module.exports = PrimeFinder;
--- test/challenge_7_spec.js
+++ test/challenge_7_spec.js
@@ -9,10 +9,19 @@ describe('PrimeFinder', function() {
 		expect(PrimeFinder).to.be.a('function');
 	});
 
+
 	describe('new instance', function(){
 		var newPrimeFinder = new PrimeFinder();
 		it('should have a method getPrimeFinder', function(){
 			expect(newPrimeFinder).to.have.property('getPrimeFinder');
+			expect(newPrimeFinder.getPrimeFinder).to.be.a('function');
+		});
+	});
+
+	describe('finds prime numbers', function(){
+		it('should find first prime number', function(){
+		var newPrimeFinder = new PrimeFinder();
+			expect(getPrimeFinder).to.equal(2);
 		});
 	});
 
